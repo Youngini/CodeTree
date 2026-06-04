422a54099b4612006bb6b5afc2c29e2b52b679f3
Youngin Kim <hardworkingini@gmail.com>
2026-06-04 13:26:10 +0900

    [Passed] 문자 출력하기 (122ms, 10MB)

---
+++ trail0/문자 출력하기/print-text.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String c = sc.next();
+        for(int i = 0; i < 8; i++) System.out.print(c);
+    }
+}
