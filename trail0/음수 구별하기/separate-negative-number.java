f107bfcf93ccbe4cbe475013ee815157ee0d2119
Youngin Kim <hardworkingini@gmail.com>
2026-06-04 10:08:03 +0900

    [Passed] 음수 구별하기 (134ms, 10MB)

---
+++ trail0/음수 구별하기/separate-negative-number.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if(N < 0) System.out.printf("%d\nminus", N);
+        else System.out.printf("%d", N);
+    }
+}
